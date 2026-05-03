f8e620e1beda42a9c6ba50d009dd2dd514c32746
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-03 21:05:52 +0000

    update file 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-__import__('add_0').add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+add = __import__('add_0').add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
