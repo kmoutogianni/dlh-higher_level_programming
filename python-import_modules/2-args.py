bac21f35205a387ceaae549f52dbc10d14a68043
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-04 09:41:39 +0000

    update file 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(num_args)) 
-        for i in (1, num_args):
+        for i in range(1, num_args + 1):
             print("{}: {}".format(i, sys.argv[i]))
                 
