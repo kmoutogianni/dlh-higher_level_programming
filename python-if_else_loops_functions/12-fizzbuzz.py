84cd5dac17b9b2e85c3add9ac3eeb64bbf219615
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-04 11:11:07 +0000

    update file 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 def fizzbuzz():
     for i in range(1, 100):
-        if i % 3 == 0and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ")
         elif i % 3 == 0:
             print("Fizz ")
