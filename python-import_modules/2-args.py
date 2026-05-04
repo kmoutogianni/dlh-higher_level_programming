494dcf7547d8687a0c7b86e29a2307015dd26290
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-04 09:44:15 +0000

    update file 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,9 @@ if __name__ == "__main__":
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-        print("1 argument:") 
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(num_args)) 
+        print("{} arguments:".format(num_args))
         for i in range(1, num_args + 1):
             print("{}: {}".format(i, sys.argv[i]))
-                
