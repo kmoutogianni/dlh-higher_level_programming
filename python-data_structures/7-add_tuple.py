6c756d77cbe43c0528a9c0771d7972a2bf9ff0cb
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-10 17:23:06 +0000

    add file 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = 0
+    a2 = 0
+    if len(tuple_a) > 0:
+        a1 = tuple_a[0]
+    if len(tuple_a) > 1:
+        a2 = tuple_a[1]
+    b1 = 0
+    b2 = 0
+    if len(tuple_b) > 0:
+        b1 = tuple_b[0]
+    if len(tuple_b) > 1:
+        b2 = tuple_b[1]
+    new_tuple = (a1 + b1, a2 + b2)
+    return new_tuple
