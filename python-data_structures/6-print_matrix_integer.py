2a5d55898eaf9881de2b920038cf98482ab8ff5a
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-10 19:59:35 +0000

    update file 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         if len(x) == 0:
-            print(" ")
+            print("")
         for i in range(len(x)):
             if i == len(x)-1:
                 print("{:d}".format(x[i]))
