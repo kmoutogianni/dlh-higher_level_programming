ffcf6f894471e1cec47352f0056523e771b2bf5a
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-10 18:45:05 +0000

    update file 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if list_length == 0:
         return None
     biggest = my_list[0]
-    for i in range(1, list_length - 1):
+    for i in range(1, list_length):
         if my_list[i] > biggest:
             biggest = my_list[i]
     return biggest
