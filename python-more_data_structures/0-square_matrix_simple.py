eea346c7bf4aefcc910ac33d935f94ab4d26620e
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-11 10:32:46 +0000

    add file 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-
+    new_matrix = []
+    for row in matrix:
+        """ for every row in the original matrix,
+        create a new row with the squared values of its elements """
+        new_row = []
+        for element in row:
+            new_row.append(element**2)
+        new_matrix.append(new_row)
+    return new_matrix
