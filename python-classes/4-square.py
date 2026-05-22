433a4d0f7fbabb14459a637ee6671773230cef44
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-22 13:05:12 +0000

    update file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
+"""Module 4"""
+
 
 class Square:
-    """Class defining a square - with calculation of area"""
-    
+    """Class defining a square - with setter/getter"""
+
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        """Returns the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Sets the size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
     def area(self):
-        """Method returning the square area"""
+        """Returns the square area"""
         return self.__size ** 2
