eee8aea58d4b31c5343df2b9eb51441692a281f6
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-22 12:25:11 +0000

    update file 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module 1"""
+
 
 class Square:
     """Class defining a square"""
 
-    def __init__(self, __size):
+    def __init__(self, size):
         self.__size = size
