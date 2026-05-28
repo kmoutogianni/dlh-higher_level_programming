4b8db0399e4a72755926b40a3bf89210337e01fa
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-28 09:19:12 +0000

    update file 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class defining a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -19,10 +19,9 @@ class Rectangle:
         """Sets the width"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+        self.__width = width
 
     @property
     def height(self):
@@ -34,10 +33,10 @@ class Rectangle:
         """Sets the height"""
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        elif height < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
+
+        self.__height = height
 
     def area(self):
         """returns the rectangle area"""
