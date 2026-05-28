6cdd054f6c158a47b7f855e1aeeee626f2d9bd41
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-28 10:12:03 +0000

    update file 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -58,7 +58,8 @@ class Rectangle:
         for h in range(self.__height):
             for w in range(self.__width):
                 rectangle = rectangle + "#"
-            rectangle = rectangle + "\n"
+            if h != self.__height - 1:
+                rectangle = rectangle + "\n"
         return rectangle
 
         def __repr__(self):
