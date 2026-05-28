bc07c43f8335aad92fcc80674ad27be4fb60fc4c
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-28 09:48:44 +0000

    add file 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -60,17 +60,3 @@ class Rectangle:
                 rectangle = rectangle + "#"
             rectangle = rectangle + "\n"
         return rectangle
-
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
