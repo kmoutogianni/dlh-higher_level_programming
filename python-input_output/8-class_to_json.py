6ac2d0aec415621a59944ef1002609734a3f0473
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-31 16:30:11 +0000

    update file 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Module Input Output"""
 
+
 def class_to_json(obj):
-    """Returns the dictionary description 
+    """Returns the dictionary description
     for JSON serialization of an object"""
 
     return obj.__dict__
