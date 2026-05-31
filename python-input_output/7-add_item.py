668464199bf561e0fd73e021124f557805cdcf84
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-31 16:08:02 +0000

    add file 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module Input Output"""
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    with open(filename, "r", encoding="utf-8") as f:
+        python_list = json.load(f)
+except FileNotFoundError:
+    python_list = []
+
+python_list.extend(sys.argv[1:])
+
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(python_list, f)
