bd2a1ec4b5bcade665302c9247edfc1e715b4134
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-31 17:36:59 +0000

    add file 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,6 +10,16 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Returns a dictionary representation of Student"""
+
+        if type(attrs) is list:
+            """If attrs is list of strings, return dictionary containing
+            only attributes contained in this list"""
+            attr_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    attr_dict[attr] = self.__dict__[attr]
+            return attr_dict
+
         return self.__dict__
