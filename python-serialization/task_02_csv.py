0e486cadc77d410e80e5441fbb3cea9023063d92
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-05-31 20:25:30 +0000

    add skeleton files for tasks 2 and 3

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import csv
+import json
+
+def convert_csv_to_json(filename):
+    """takes the CSV filename as its parameter and writes the JSON data to data.json"""
+    pass
+
+
---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as etree
+
+
+def serialize_to_xml(dictionary, filename):
+    pass
+
+def deserialize_from_xml(filename):
+    pass
+
+
+
