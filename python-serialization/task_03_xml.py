24392867feae8fa072f84ef43417cefd534b0d02
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-06-01 16:16:31 +0200

    add file 03

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
-import xml.etree.ElementTree as etree
+import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
-    pass
+    """serialize Python dictionary to XML file"""
+
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
 
 def deserialize_from_xml(filename):
-    pass
+    """deserialize an XML file to a Python dictionary"""
+    tree = ET.parse(filename)
+    root = tree.getroot()
 
+    Pyth_dictionary = {}
 
+    for child in root:
+        print(child)
+        key = child.tag
+        value = child.text
 
+        Pyth_dictionary[key] = value
+
+    return Pyth_dictionary
