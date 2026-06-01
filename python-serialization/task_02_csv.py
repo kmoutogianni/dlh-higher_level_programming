38b435d15eb5b6a4d5d7ce4f6744e9027e57148f
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-06-01 15:20:52 +0200

    add file task02

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,8 +2,18 @@
 import csv
 import json
 
+
 def convert_csv_to_json(filename):
-    """takes the CSV filename as its parameter and writes the JSON data to data.json"""
-    pass
+    """reads from CSV filename and writes the JSON data to data.json"""
+    try:
+        with open(filename, "r", encoding="utf-8") as input_csv_file:
+            input_data = csv.DictReader(input_csv_file)  # DictReader obj/rows
+            data = list(input_data)
 
+        with open("data.json", "w", encoding="utf-8") as output_json_file:
+            json.dump(data, output_json_file)
 
+        return True
+
+    except Exception:
+        return False
